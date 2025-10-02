d88eec5a21f50588410799b55b518d327fedcc25
Michael Opare Baah <109297033+MichaelOpareBaah@users.noreply.github.com>
2025-10-02 17:11:15 +0000

    Initial commit

---
+++ src/component/Counter.jsx
@@ -0,0 +1,39 @@
+import React, {  useState } from 'react'
+
+export default function Counter() {
+  const[count, setCount]=useState(0)
+  const[saveCount, setSaveCount]= useState([])
+  function handleIncrement(){
+    setCount(item=>item+1)
+   }
+  function handleDecrement(){
+     if(count>0){
+      setCount(item=>item-1)
+    } else{
+      alert('Count is zero and can not be decreased')
+    }
+   }
+    function handleSave(){
+        if(count===0){
+             alert('Count is zero hence can not be saved')
+        }else{
+            setSaveCount(item=>[...item, count + ", "])
+            setCount(0)
+         }
+    }
+    // localStorage.setItem('countLocal',saveCount);
+    // const showStoredItems= localStorage.getItem('countLocal')
+  return (
+    <section className='container' >
+      <h1>Counter App</h1>
+      <p className='count'>{count}</p>
+      <div className='btns'>
+        <button className='increment-btn' onClick={handleIncrement}>Increment</button>
+        <button className='decrement-btn' onClick={handleDecrement}>Decrement</button>
+      </div>
+      <button className='save-btn' onClick={handleSave}>Save Count</button>
+      <p className='saved-items'>Saved Counts:{saveCount}</p>
+      
+    </section>
+  )
+}
